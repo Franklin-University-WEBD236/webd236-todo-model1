a37d70c38cdf4bdaf342136222cd416d20ec8f47
Glitch (tart-cemetery) <none>
2019-05-19 20:32:45 +0000

    🚜👘 Checkpoint ./index.php:86128/1327

--- index.php
+++ index.php
@@ -2,24 +2,26 @@
 <html>
   <head>
     <title>BMI Calculator</title>
-    <link rel="stylesheet" href="static/style.css" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div id="content">
+    <div class="jumbotron text-center">
       <h1>BMI Calculator</h1>
-      <p><em>Author: Todd Whittaker</em></p>
+      <p><em>Author: <a href="https://www.franklin.edu/about-us/faculty-staff/faculty-profiles/whittakt">Todd Whittaker</a></em></p>
+    </div>
+    <div class="container">
+      <div class="row">
       <p>This program will calculate your body mass index and
          indicate what your range is.</p>
+        
       <form action="bmi.php" method="post">
-        <fieldset>
-          <legend>Input your data</legend>
-          <label for="height">Height (inches):</label>
-          <input type="text" id="height" name="height" /><br />
-          <label for="height">Weight (pounds):</label>
-          <input type="text" id="weight" name="weight" /><br />
-          <label>&nbsp;</label>
-          <input type="submit" value="Submit" /><br />
-        </fieldset>
+        <label for="height">Height (inches):</label>
+        <input type="text" id="height" name="height" /><br />
+        <label for="height">Weight (pounds):</label>
+        <input type="text" id="weight" name="weight" /><br />
+        <label>&nbsp;</label>
+        <input type="submit" value="Submit" /><br />
       </form>
     </div>
   </body>
