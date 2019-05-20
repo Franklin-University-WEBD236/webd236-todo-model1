41d5873050c0d12ea32962413c1b3403492c2b6d
Glitch (tart-cemetery) <none>
2019-05-20 13:32:15 +0000

    😅🍐 Checkpoint ./index.php:86128/930 ./bmi.php:86128/1486

--- index.php
+++ index.php
@@ -6,26 +6,26 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class="jumbotron">
-      <div class="col-lg-8 offset-2">
-        <h1 class="display-4">PHP BMI Calculator</h1>
-        <p class="lead">Calculate your body mass index</p>
-        <hr class="my-4">
-        <p><em>Author: <a href="https://www.franklin.edu/about-us/faculty-staff/faculty-profiles/whittakt">Todd Whittaker</a></em></p>
-      </div>
-    </div>
     <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-2">
+          <h1 class="display-4">PHP BMI Calculator</h1>
+          <p class="lead">Calculate your body mass index</p>
+          <p><em>Author: <a href="https://www.franklin.edu/about-us/faculty-staff/faculty-profiles/whittakt">Todd Whittaker</a></em></p>
+          <hr class="my-4">
+        </div>
+      </div>
       <div class="row">
         <div class="col-lg-8 offset-2">
           <p>This program will calculate your body mass index and indicate what your range is.</p>
           <form action="bmi.php" method="post">
             <div class="form-group">
               <label for="height">Height (inches)</label>
-              <input type="text" id="height" name="height" class="form-control" placeholder="Enter height" />
+              <input type="number" min="1" id="height" name="height" class="form-control" placeholder="Enter height" />
             </div>
             <div class="form-group">
               <label for="height">Weight (pounds)</label>
-              <input type="text" id="weight" name="weight" class="form-control" placeholder="Enter weight" />
+              <input type="number" min="1" id="weight" name="weight" class="form-control" placeholder="Enter weight" />
             </div>
             <div class="form-group">
               <button type="submit" class="btn btn-primary">Submit</button>
