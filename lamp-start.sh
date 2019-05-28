2cc7a7f49dc01b694b47e9d937ec6be8618dad15
Glitch (tart-cemetery) <none>
2019-05-28 15:30:59 +0000

    🏯😀 Checkpoint ./lamp-start.sh:86128/82

--- lamp-start.sh
+++ lamp-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 truncate -s 0 .apache2/log/*
-.mysql/run-mysqld.sh &
+#.mysql/run-mysqld.sh &
+rm MicroUniversity.db3
+sqlite3 MicroUniversity.db3 < MicroUniversity.sql
 .apache2/run-apache2.sh &
 
 tail -f -v .apache2/log/*
