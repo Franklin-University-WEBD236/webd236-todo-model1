627fe38fdd651bc14de54fdd23b97d826cb3a2ee
Glitch (tart-cemetery) <none>
2019-05-28 01:14:50 +0000

    🐺🐮 Checkpoint ./view.php:86128/3493

--- view.php
+++ view.php
@@ -7,62 +7,31 @@
   $advisor = findEmployeeById($student['EMP_NUM']);
 
 
-  function makeTextInput($fieldname, $label) {
-    echo "<div class=\"form-group\">\n"
-    echo "<label for=\"stu_fname\">First name</label>"
-    echo "<input type=\"text\" id=\"stu_fname\" name=\"stu_fname\" class=\"form-control\" placeholder=\"Enter first name" value=\"{$student['STU_FNAME']}" ?>"/>
+  function makeTextInput($arr, $fieldname, $label, $disabled="disabled") {
+    echo "<div class=\"form-group\">\n";
+    echo "<label for=\"{$fieldname}\">{$label}</label>\n";
+    echo "<input type=\"text\" id=\"stu_fname\" name=\"{$fieldname}\" class=\"form-control\" placeholder=\"Enter {$label}\" value=\"{$arr[$fieldname]}\" {$disabled} />\n";
+    echo "</div>";
   }
+
   include('parts/header.php');
 ?>
 
       <div class="row">
         <div class="col-lg-8 offset-2">
           <form action="index.php" method="post">
-            <div class="form-group">
-              <label for="stu_fname">First name</label>
-              <input type="text" min="1" id="stu_fname" name="stu_fname" class="form-control" placeholder="Enter first name" value="<?php echo "{$student['STU_FNAME']}" ?>"/>
-            </div>
-            <div class="form-group">
-              <label for="last_name">Last name</label>
-              <input type="text" min="1" id="last_name" name="last_name" class="form-control" placeholder="Enter last name" value="<?php echo "{$student['STU_LNAME']}" ?>"/>
-            </div>
-            <div class="form-group">
-              <label for="last_name">Last name</label>
-              <input type="text" min="1" id="initial" name="initial" class="form-control" placeholder="Enter last name" value="<?php echo "{$student['STU_INITIAL']}" ?>"/>
-            </div>
-            <div class="form-group">
-              <label for="last_name">Last name</label>
-              <input type="text" min="1" id="last_name" name="last_name" class="form-control" placeholder="Enter last name" value="<?php echo "{$student['STU_LNAME']}" ?>"/>
-            </div>
-            <div class="form-group">
-              <label for="last_name">Last name</label>
-              <input type="text" min="1" id="last_name" name="last_name" class="form-control" placeholder="Enter last name" value="<?php echo "{$student['STU_LNAME']}" ?>"/>
-            </div>
+<?php
+  makeTextInput($student, "STU_FNAME", "First name");
+  makeTextInput($student, "STU_LNAME", "Last name");
+  makeTextInput($student, "STU_INITIAL", "Middle initial");
+  makeTextInput($student, "STU_DOB", "Date of birth");
+  makeTextInput($student, "STU_HRS", "Hours");
+  makeTextInput($student, "STU_CLASS", "Class");
+  makeTextInput($student, "DEPT_CODE", "Department code");
+  makeTextInput($student, "STU_PHONE", "Phone number");
+?>
           </form>
+    <a href="."><< Back</a>
         </div>
       </div>
-                
-<h1>View a student</h1>
-<div class='display'>
-    <label>First name:</label>
-    <div><?php echo "{$student['STU_FNAME']}"; ?></div>
-    <label>Last name:</label>
-    <div><?php echo "{$student['STU_LNAME']}"; ?></div>
-    <label>Middle initial:</label>
-    <div><?php echo "{$student['STU_INITIAL']}"; ?></div>
-    <label>Birthdate:</label>
-    <div><?php echo "{$student['STU_DOB']}"; ?></div>
-    <label>Credits:</label>
-    <div><?php echo "{$student['STU_HRS']}"; ?></div>
-    <label>Class:</label>
-    <div><?php echo "{$student['STU_CLASS']}"; ?></div>
-    <label>Department:</label>
-    <div><?php echo "{$student['DEPT_CODE']}"; ?></div>
-    <label>Phone:</label>
-    <div><?php echo "{$student['STU_PHONE']}"; ?></div>
-    <label>Advisor:</label>
-    <div><?php echo "{$advisor['EMP_FNAME']} {$advisor['EMP_LNAME']}"; ?></div>
-</div>
-    <a href="."><< Back</a>
-
 <?php include('parts/footer.php'); ?>
